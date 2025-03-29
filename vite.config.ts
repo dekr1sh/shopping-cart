ad74758e2f72a83a4f93c0f07ccc125f57c6ed6a
dekr1sh <krishsonigra2175@gmail.com>
2025-03-29 18:53:14 +0530

    Resolve the path for `./src` using the alias `@`

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +12,10 @@ export default defineConfig({
   test: {
     environment: "jsdom", 
     setupFiles: "./src/__tests__/setup.ts", 
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 })
