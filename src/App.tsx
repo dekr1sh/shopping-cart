40b92f197bc0fccdb6a8e8c913f10c60a6622fef
dekr1sh <krishsonigra2175@gmail.com>
2025-03-31 16:08:47 +0530

    Fix typo in component's name

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom"
 import Navbar from "./components/Navbar.tsx";
 import Footer from "./components/Footer.tsx";
 
-export default function RootLayout() {
+export default function App() {
   return (
     <div className="flex min-h-screen flex-col">
       <Navbar />
