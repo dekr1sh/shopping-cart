c8a03051aeb47ab01abb4a505982ce90679efb4b
dekr1sh <krishsonigra2175@gmail.com>
2025-04-02 15:35:10 +0530

    Remove the test for verifying the navigation due to the `Link` element
    We were unnecessarily testing `react-router-dom`'s `Link` element
    even though is it already tested.

--- src/__tests__/pages/NotFoundPage.test.tsx
+++ src/__tests__/pages/NotFoundPage.test.tsx
@@ -1,6 +1,4 @@
 import { describe, it, expect } from "vitest";
-import { screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import { render } from "../test-utils";
 import NotFoundPage from "@/pages/NotFoundPage";
 
@@ -9,14 +7,4 @@ describe("NotFoundPage", () => {
     const { container } = render(<NotFoundPage />);
     expect(container).toMatchSnapshot();
   });
-
-  it("navigates to home page when button is clicked", async () => {
-    const user = userEvent.setup();
-
-    render(<NotFoundPage />, { route: "/random-page" });
-    const link = screen.getByRole("link", { name: /go home/i });
-
-    await user.click(link);
-    expect(window.location.pathname).toBe("/");
-  })
 });
