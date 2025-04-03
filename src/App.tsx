4a3f664edf2584d46d8145279cbbf2b4507a5d78
dekr1sh <krishsonigra2175@gmail.com>
2025-04-03 23:31:20 +0530

    Update styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,7 @@ export default function App() {
   return (
     <div className="flex min-h-screen flex-col">
       <Navbar />
-      <main className="flex-1">
-        <Outlet />
-      </main>
+      <Outlet />
       <Footer />
     </div>
   )
