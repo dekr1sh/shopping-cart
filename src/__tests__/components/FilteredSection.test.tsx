bc48faf07dfaeb721d8e1f7059c558d9dea7054b
dekr1sh <krishsonigra2175@gmail.com>
2025-04-05 01:36:13 +0530

    Write tests for `FilteredSection` component and also add snapshots

---
+++ src/__tests__/components/FilteredSection.test.tsx
@@ -0,0 +1,45 @@
+import { describe, it, expect, vi } from "vitest";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { screen } from "@testing-library/dom";
+import FilterSection from "@/components/FilterSection";
+
+describe("FilterSection", () => {
+  const mockHandleBrandChange = vi.fn();
+  const mockSetMinPrice = vi.fn();
+  const mockSetMaxPrice = vi.fn();
+  const mockClearFilters = vi.fn();
+
+  const defaultProps = {
+    selectedBrands: [],
+    minPrice: undefined,
+    maxPrice: undefined,
+    onBrandChange: mockHandleBrandChange,
+    onMinPriceChange: mockSetMinPrice,
+    onMaxPriceChange: mockSetMaxPrice,
+    onClearFilters: mockClearFilters,
+  };
+
+  it("renders correctly", () => {
+    const { container } = render(<FilterSection {...defaultProps} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it.only("calls onMinPriceChange when the min price input changes", async () => {
+    render(<FilterSection {...defaultProps} />);
+
+    const minPriceInput = screen.getByPlaceholderText("Min");
+    await userEvent.type(minPriceInput, "1");
+
+    expect(mockSetMinPrice).toHaveBeenCalledWith(1);
+  });
+
+  it("calls onMaxPriceChange when the max price input changes", async () => {
+    render(<FilterSection {...defaultProps} />);
+
+    const maxPriceInput = screen.getByPlaceholderText("Max");
+    await userEvent.type(maxPriceInput, "5");
+
+    expect(mockSetMaxPrice).toHaveBeenCalledWith(5);
+  });
+});
