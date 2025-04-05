600142c5bc658ce9384e5a16fba5bdc535f2d92a
dekr1sh <krishsonigra2175@gmail.com>
2025-04-05 22:52:19 +0530

    Call `Toaster` component inside of `App`
    It is called inside of `App` so that when it is called from anywhere,
    it actually gets rendered since `App` is always rendered.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "./components/Navbar.tsx";
 import Footer from "./components/Footer.tsx";
+import { Toaster } from "sonner"
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Navbar />
       <Outlet />
       <Footer />
+      <Toaster richColors position="bottom-right" />
     </div>
   )
 }
